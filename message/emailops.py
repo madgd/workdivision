a02d2954a44553c5996e866214d1587155bf4bff
madgd <iamnushead@gmail.com>
2021-11-19 18:28:00 +0800

    add from email box choice

--- message/emailops.py
+++ message/emailops.py
@@ -62,14 +62,16 @@ class EmailSender:
 
     """
     smtp = None
-    def __init__(self):
+    def __init__(self, conf_sec=ConfigSec):
         # config
         config = configparser.ConfigParser()
         config.read(EmailConfig)
-        self.host_server = config.get(ConfigSec, "host_server")
-        self.fromaddress = config.get(ConfigSec, "fromaddress")
-        self.password = config.get(ConfigSec, "password")
-        self.inport = config.get(ConfigSec, "inport")
+        if not conf_sec:
+            conf_sec = ConfigSec
+        self.host_server = config.get(conf_sec, "host_server")
+        self.fromaddress = config.get(conf_sec, "fromaddress")
+        self.password = config.get(conf_sec, "password")
+        self.inport = config.get(conf_sec, "inport")
         try:
             self.smtp = SMTP_SSL(self.host_server)
             self.smtp.set_debuglevel(0)
@@ -123,7 +125,7 @@ class EmailSender:
         retry = 0
         while retry < 3:
             try:
-                self.smtp.sendmail(self.fromaddress, addresses + cc, msg.as_string())
+                # self.smtp.sendmail(self.fromaddress, addresses + cc, msg.as_string())
                 print("[emailops][email sent][tm=%s][retry=%d]host:%s, port:%s, from:%s, to:%s, cc:%s, subject:%s, attach:%s"
                   % (f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S}", retry, self.host_server, self.inport, self.fromaddress, msg["to"], msg["cc"], msg["subject"],
                      attach_names)
@@ -167,7 +169,7 @@ class EmailSender:
         return False
 
 
-def sendEmail(addresses, content, mail_title, attach, cc=[]):
+def sendEmail(addresses, content, mail_title, attach, cc=[], conf_sec=ConfigSec):
     """
 
     :param addresses: To list
@@ -179,10 +181,12 @@ def sendEmail(addresses, content, mail_title, attach, cc=[]):
     # config
     config = configparser.ConfigParser()
     config.read(EmailConfig)
-    host_server = config.get(ConfigSec, "host_server")
-    fromaddress = config.get(ConfigSec, "fromaddress")
-    password = config.get(ConfigSec, "password")
-    inport = config.get(ConfigSec, "inport")
+    if not conf_sec:
+        conf_sec = ConfigSec
+    host_server = config.get(conf_sec, "host_server")
+    fromaddress = config.get(conf_sec, "fromaddress")
+    password = config.get(conf_sec, "password")
+    inport = config.get(conf_sec, "inport")
 
     # email content
     mail_content = content
@@ -223,7 +227,7 @@ def sendEmail(addresses, content, mail_title, attach, cc=[]):
             smtp.set_debuglevel(0)
             smtp.ehlo(host_server)
             smtp.login(fromaddress, password)
-            smtp.sendmail(fromaddress, addresses + cc, msg.as_string())
+            # smtp.sendmail(fromaddress, addresses + cc, msg.as_string())
             smtp.quit()
             print("[emailops][email sent][tm=%s][retry=%d]host:%s, port:%s, from:%s, to:%s, cc:%s, subject:%s, attach:%s"
               % (f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S}", retry, host_server, inport, fromaddress, msg["to"], msg["cc"], msg["subject"],
@@ -240,81 +244,83 @@ def sendEmail(addresses, content, mail_title, attach, cc=[]):
             time.sleep(2 + retry)
 
 
-def receiveEmail():
-    # config
-    config = configparser.ConfigParser()
-    config.read(EmailConfig)
-    host_server = config.get(ConfigSec, "host_server")
-    fromaddress = config.get(ConfigSec, "fromaddress")
-    password = config.get(ConfigSec, "password")
-    inport = config.get(ConfigSec, "inport")
-    print(host_server, fromaddress, password, inport)
-
-    # receive
-    conn = imaplib.IMAP4_SSL(port=inport, host=host_server)
-    conn.login(fromaddress, password)
-    conn.select()
-    count = len(conn.search(None, 'ALL')[1][0].split())
-    # read last one
-    typ, email_content = conn.fetch(f'{count}'.encode(), '(RFC822)')
-    # byte 2 str
-    print(email_content)
-    print((email_content[1]))
-    print(email_content[0][0])
-    email_content = email_content[0][1].decode()
-    print(email_content)
-    conn.close()
-    conn.logout()
-
-def receiveEmail2():
-    # config
-    config = configparser.ConfigParser()
-    config.read(EmailConfig)
-    email = config.get(ConfigSec, "fromaddress")
-    password = config.get(ConfigSec, "password")
-    pop3_server = config.get(ConfigSec, "host_server")
-
-    # 连接到POP3 服务器
-    server = poplib.POP3_SSL(pop3_server)
-    # 可以打开或关闭调试信息
-    server.set_debuglevel(1)
-    # 可选：输出POP3服务器的欢迎文字
-    print(server.getwelcome().decode('utf-8'))
-
-    # 身份认证
-    server.user(email)
-    server.pass_(password)
-    # stat()返回邮件数量和占用空间
-    print('Messages: %s. Size: %s' % server.stat())
-    # list()返回所有邮件的编号
-    resp, mails, octets = server.list()
-    # 可以查看返回的列表，类似[b'1 82923', b'2 2184', ...]
-    print(mails)
-
-    # 获取最新一封邮件, 注意索引号从1 开始
-    index = len(mails)
-    print(index)
-    resp, lines, octets = server.retr(index)
-
-    # lines 存储了邮件原始文本的每一行
-    # 可以获得整个邮件的原始文本
-    msg_content = b'\r\n'.join(lines).decode('utf-8')
-    # 稍后解析邮件
-    msg = Parser().parsestr(msg_content)
-
-    # 可以根据邮件索引号直接从服务器删除邮件
-    # server.dele(index)
-    # 关闭连接
-    server.quit()
+# def receiveEmail():
+#     # config
+#     config = configparser.ConfigParser()
+#     config.read(EmailConfig)
+#     host_server = config.get(ConfigSec, "host_server")
+#     fromaddress = config.get(ConfigSec, "fromaddress")
+#     password = config.get(ConfigSec, "password")
+#     inport = config.get(ConfigSec, "inport")
+#     print(host_server, fromaddress, password, inport)
+#
+#     # receive
+#     conn = imaplib.IMAP4_SSL(port=inport, host=host_server)
+#     conn.login(fromaddress, password)
+#     conn.select()
+#     count = len(conn.search(None, 'ALL')[1][0].split())
+#     # read last one
+#     typ, email_content = conn.fetch(f'{count}'.encode(), '(RFC822)')
+#     # byte 2 str
+#     print(email_content)
+#     print((email_content[1]))
+#     print(email_content[0][0])
+#     email_content = email_content[0][1].decode()
+#     print(email_content)
+#     conn.close()
+#     conn.logout()
+#
+# def receiveEmail2():
+#     # config
+#     config = configparser.ConfigParser()
+#     config.read(EmailConfig)
+#     email = config.get(ConfigSec, "fromaddress")
+#     password = config.get(ConfigSec, "password")
+#     pop3_server = config.get(ConfigSec, "host_server")
+#
+#     # 连接到POP3 服务器
+#     server = poplib.POP3_SSL(pop3_server)
+#     # 可以打开或关闭调试信息
+#     server.set_debuglevel(1)
+#     # 可选：输出POP3服务器的欢迎文字
+#     print(server.getwelcome().decode('utf-8'))
+#
+#     # 身份认证
+#     server.user(email)
+#     server.pass_(password)
+#     # stat()返回邮件数量和占用空间
+#     print('Messages: %s. Size: %s' % server.stat())
+#     # list()返回所有邮件的编号
+#     resp, mails, octets = server.list()
+#     # 可以查看返回的列表，类似[b'1 82923', b'2 2184', ...]
+#     print(mails)
+#
+#     # 获取最新一封邮件, 注意索引号从1 开始
+#     index = len(mails)
+#     print(index)
+#     resp, lines, octets = server.retr(index)
+#
+#     # lines 存储了邮件原始文本的每一行
+#     # 可以获得整个邮件的原始文本
+#     msg_content = b'\r\n'.join(lines).decode('utf-8')
+#     # 稍后解析邮件
+#     msg = Parser().parsestr(msg_content)
+#
+#     # 可以根据邮件索引号直接从服务器删除邮件
+#     # server.dele(index)
+#     # 关闭连接
+#     server.quit()
 
 # fixme: imbox should use master branch
-def receiveEmail3(**kwargs):
+def receiveEmail3(conf_sec=ConfigSec, **kwargs):
     # config
     config = configparser.ConfigParser()
     config.read(EmailConfig)
-    email = config.get(ConfigSec, "fromaddress")
-    password = config.get(ConfigSec, "password")
-    server = config.get(ConfigSec, "host_server")
+    if not conf_sec:
+        conf_sec = ConfigSec
+    email = config.get(conf_sec, "fromaddress")
+    password = config.get(conf_sec, "password")
+    server = config.get(conf_sec, "host_server")
     with Imbox(server, email, password, ssl=True) as imbox:
         # read messages
         all_inbox_messages = imbox.messages(**kwargs)
@@ -338,15 +344,15 @@ if __name__ == '__main__':
     # for i in range(50):
     #     sendEmail(addresses, content, Subject, attach, cc)
 
-    sender = EmailSender()
-    for i in range(200):
-        sender.sendEmail(addresses, content, Subject, attach, cc)
-        time.sleep(1)
+    # sender = EmailSender('DEFAULT')
+    # for i in range(1):
+    #     sender.sendEmail(addresses, content, Subject, attach, cc)
+    #     time.sleep(1)
     # receiveEmail()
     # receiveEmail2()
-    # res = receiveEmail3(uid__range='1577703015:*')
-    # for uid, message in res:
-    #     print(uid)
-    #     print(message.sent_from)
-    #     for attach in message.attachments:
-    #         print(attach['filename'])
+    res = receiveEmail3('gszl1', uid__range='2577703055:*')
+    for uid, message in res:
+        print(uid)
+        print(message.sent_from)
+        for attach in message.attachments:
+            print(attach['filename'])
